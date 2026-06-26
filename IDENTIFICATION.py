7219abcb33cd5288d5466fe1a8607b7dfbc5a254
GBOLAHN400 <110310164+Jvvjjjhhgg@users.noreply.github.com>
2026-06-26 13:56:21 +0100

    Create IDENTIFICATION.py

---
+++ IDENTIFICATION.py
@@ -0,0 +1,8 @@
+username = input("Enter your username: ")
+pwd = input("Enter your Password: ")
+
+if username == "Shola" && pwd == "pass123":
+  print("You enter valid identification!!!")
+
+else:
+  print("You Enter invalid identification, try again!!!")
